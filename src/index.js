bbed3c06169e2ee0c2bcc5e820c24f74820ea3b2
MVN <gormaster@mail.ru>
2021-08-20 11:32:15 +0400

    Clean Up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import moment from "moment";
+// import { range } from "moment-range";
+import "./index.css";
+import "./calendar.css";
+
+
 
 ReactDOM.render(
   <React.StrictMode>
