c3462465b1e27e05e553b5c8add00c551de546bd
ZarNizza <gormaster@mail.ru>
2021-08-21 23:04:14 +0400

    toDo parsing - draft

--- src/index.js
+++ src/index.js
@@ -24,22 +24,23 @@ function ToDo() {
   }
 
   function addItem() {
-    setTodos([...todos, someString]);
+    let toDo = parseToDo(someString);
+    setTodos([...todos, toDo]);
     setSomeString("");
   }
 
   function handleKeyPress(e) {
     if (e.key === "Enter") {
-      let toDo = parceToDo(e.target.value);
+      let toDo = parseToDo(e.target.value);
       setSomeString(toDo.toString());
       setTodos([...todos, toDo]);
       setSomeString("");
     }
   }
 
-  function parceToDo(draftText) {
+  function parseToDo(draftText) {
     let toDo = {
-      date: "31/12",
+      date: "01/03",
       day: 31,
       month: 11,
       year: 2021,
@@ -49,23 +50,24 @@ function ToDo() {
     };
 
     if(draftText) {
-    toDo.date=draftText.match(/(\d\d\/\d\d)|(\d\d\.\d\d)/i);
-    toDo.time=draftText.match(/(\d\d\-\d\d)|(\d\d\:\d\d)/i);
-//    toDo.who=draftText.match(/\s-(\w)[^\d]{3,}-\s/i);
-//    toDo.who=draftText.substr(draftText.indexOf(" - "));
+    toDo.date = (draftText.match(/(\d\d\/\d\d)|(\d\d\.\d\d)/i)) ? draftText.match(/(\d\d\/\d\d)|(\d\d\.\d\d)/i)[0] : ["01/00"];
+    toDo.time = (draftText.match(/(\d\d\-\d\d)|(\d\d\:\d\d)/i)) ? draftText.match(/(\d\d\-\d\d)|(\d\d\:\d\d)/i)[0] : ["10:00"];
+    toDo.who=draftText.slice(draftText.indexOf("- ") + 2);
+    toDo.whatWhere=toDo.who.slice(toDo.who.indexOf("- ") + 2);
+    toDo.who=toDo.who.slice(0, toDo.who.indexOf("- ") - 1);
 
-    toDo.day = +toDo.date[0].slice(0,2);
-    toDo.month = +toDo.date[0].slice(3,5)-1;
+    toDo.day = +toDo.date.slice(0,2);
+    toDo.month = +toDo.date.slice(3,5)-1;
     }
-    console.log("time=" + toDo.time[0].toString() + "=");
-    console.log("date=" + toDo.date[0].toString() + "= d=" + toDo.day + "= m=" + toDo.month);
-    console.log("who=" + toDo.who[0].toString() + "=");
-    console.log("whatWhere=" + toDo.whatWhere[0].toString() + "=");
-    
+/*     console.log("time=" + toDo.time[0].toString() + "=");
+    console.log("date=" + toDo.date[0].toString() + "= d=" + toDo.day + "= m=" + toDo.month + "=");
+    console.log("who=" + toDo.who + "=");
+    console.log("whatWhere=" + toDo.whatWhere + "=");
+ */    
     return toDo;
   }
 
-  let defText = "[дата ДД/ММ ][время чч:мм -][ кто -] что,где";
+  let defText = "[дата ДД/ММ ][время чч:мм ] [- кто -] что,где";
 
   return (
     <div>
@@ -76,7 +78,7 @@ function ToDo() {
       <br />
       {todos.map((todo) => (
         <li key={todo.date + todo.time}>
-          {todo.date + " " + todo.time + " - " + todo.who + " - " + todo.whatWhere}
+          {todo.day + "/" + todo.month + " " + todo.time + " - " + todo.who + " - " + todo.whatWhere}
         </li>
       ))}
     </div>
