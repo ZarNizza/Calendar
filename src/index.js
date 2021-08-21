5ffb08dc325fa57916cb8b8d06fe8bfca27e457d
ZarNizza <gormaster@mail.ru>
2021-08-21 19:32:59 +0400

    parsing date/time

--- src/index.js
+++ src/index.js
@@ -39,11 +39,29 @@ function ToDo() {
 
   function parceToDo(draftText) {
     let toDo = {
-      date: "11/12",
+      date: "31/12",
+      day: 31,
+      month: 11,
+      year: 2021,
       time: "21-15",
       who: "Luke SkyWalker",
       whatWhere: "Party at BlackStarCarnegieHall",
     };
+
+    if(draftText) {
+    toDo.date=draftText.match(/(\d\d\/\d\d)|(\d\d\.\d\d)/i);
+    toDo.time=draftText.match(/(\d\d\-\d\d)|(\d\d\:\d\d)/i);
+//    toDo.who=draftText.match(/\s-(\w)[^\d]{3,}-\s/i);
+//    toDo.who=draftText.substr(draftText.indexOf(" - "));
+
+    toDo.day = +toDo.date[0].slice(0,2);
+    toDo.month = +toDo.date[0].slice(3,5)-1;
+    }
+    console.log("time=" + toDo.time[0].toString() + "=");
+    console.log("date=" + toDo.date[0].toString() + "= d=" + toDo.day + "= m=" + toDo.month);
+    console.log("who=" + toDo.who[0].toString() + "=");
+    console.log("whatWhere=" + toDo.whatWhere[0].toString() + "=");
+    
     return toDo;
   }
 
