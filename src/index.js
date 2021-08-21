96b1637de5c3281c714a2352feec100d7b7b889b
ZarNizza <gormaster@mail.ru>
2021-08-21 17:51:45 +0400

    toDoParcer plug

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 // Calendar - pure REACT version, "my own bike ))"
 // skeleton edition
 
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import CalendarTable from "./calendarMatrix.js"
+import CalendarTable from "./calendarMatrix.js";
 import "./index.css";
-
 
 function CalendarHeader() {
   return (
@@ -17,35 +16,54 @@ function CalendarHeader() {
 }
 
 function ToDo() {
-  const [someString, setSomeString] = useState('');
+  const [someString, setSomeString] = useState("");
   const [todos, setTodos] = useState([]);
 
   function handleChange(e) {
-    setSomeString(e.target.value)
+    setSomeString(e.target.value);
   }
 
   function addItem() {
     setTodos([...todos, someString]);
-    setSomeString('')
+    setSomeString("");
   }
 
   function handleKeyPress(e) {
-    if(e.key === 'Enter'){
-    setSomeString(e.target.value);
-    setTodos([...todos, someString]);
-    setSomeString('')
-    } 
+    if (e.key === "Enter") {
+      let toDo = parceToDo(e.target.value);
+      setSomeString(toDo.toString());
+      setTodos([...todos, toDo]);
+      setSomeString("");
+    }
+  }
+
+  function parceToDo(draftText) {
+    let toDo = {
+      date: "11/12",
+      time: "21-15",
+      who: "Luke SkyWalker",
+      whatWhere: "Party at BlackStarCarnegieHall",
+    };
+    return toDo;
   }
 
   let defText = "[дата ДД/ММ ][время чч:мм -][ кто -] что,где";
 
-  return <div>
-    <p>{defText}</p>
-     <input value={someString} onChange={handleChange} onKeyPress={handleKeyPress} /> <button onClick={addItem}>add (+)</button>
-      {todos.map(todo => <li key={todo.toString().slice(0,11)}>{todo}</li>)}
-  </div>
+  return (
+    <div>
+      <p><i>{defText}</i></p>
+      <input value={someString} onChange={handleChange} onKeyPress={handleKeyPress} /> &nbsp;
+      <button onClick={addItem}>add (+)</button>
+      <br />
+      <br />
+      {todos.map((todo) => (
+        <li key={todo.date + todo.time}>
+          {todo.date + " " + todo.time + " - " + todo.who + " - " + todo.whatWhere}
+        </li>
+      ))}
+    </div>
+  );
 }
-
 
 function Span() {
   return <p> - - - - - </p>;
